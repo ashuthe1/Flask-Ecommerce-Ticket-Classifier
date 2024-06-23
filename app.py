fd22008862c02ca2293e67b529a08650df03e2ed
Ashutosh Gautam <ashutosh251848@gmail.com>
2024-06-23 19:19:55 +0530

    Integrated Lllam3 Model for classification

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
 import requests
 import json
-from dotenv import load_dotenv
-import os
+from ollama import Client
 
 load_dotenv()
 
 app = Flask(__name__)
 
-API_KEY = os.getenv('GOOGLE_API_KEY')
-API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent'
-
 categories = ["Payment", "Delivery", "Product/Merchant", "Satisfied"]
 
+def classify_review_llama(text):
+    client = Client(host='http://127.0.0.1:11434')
+    
+    prompt = f"Classify the given message into one of the following categories: {', '.join(categories)}. Message: \"{text}\". Return the final classification only."
+    
+    messages = [
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+    ]
+    
+    response = client.chat(model='llama3', messages=messages)
+    
+    classification = response['message']['content'].strip()
+    return classification
+
 def classify_review(text):
+
+    API_KEY = os.getenv('GOOGLE_API_KEY')
+    API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent'
+
     url = f"{API_URL}?key={API_KEY}"
     prompt = f"Classify the given message into one of the following categories: {', '.join(categories)}. Message: \"{text}\". Return the final classification only."
     
@@ -36,7 +55,7 @@ def classify_review(text):
     response = requests.post(url, headers=headers, data=json.dumps(payload))
 
     if response.status_code != 200:
-        raise Exception(f"Google PaLM2 API returned non-200 status: {response.status_code}")
+        raise Exception(f"Gemini returned non-200 status: {response.status_code}")
 
     result = response.json()
 
@@ -68,6 +87,23 @@ def classify_review_endpoint():
     
     try:
         classification = classify_review(review_text)
+        return jsonify({"classification": classification})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/classify_review_llama', methods=['POST'])
+def classify_review_llama_endpoint():
+    if not request.is_json:
+        return jsonify({"error": "Invalid input"}), 400
+    
+    data = request.get_json()
+    review_text = data.get('text')
+    
+    if not review_text:
+        return jsonify({"error": "Text is required"}), 400
+    
+    try:
+        classification = classify_review_llama(review_text)
         return jsonify({"classification": classification})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
